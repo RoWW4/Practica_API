fbc3f9ff2f01aa56f4774502783bed8d56dd738c
Rocío Beato Martín <rociobeato91@gmail.com>
2021-04-05 12:17:33 +0200

    carga video del input

--- javascript.js
+++ javascript.js
@@ -1,12 +1,30 @@
-if(window.File && window.FileReader){
+if (window.File && window.FileReader && window.FileList) {
     console.log('Todas las API son soportadas');
-}
-else{
+} else {
     alert('La API de FILE no es soportada en este navegador');
 }
 
-function archivo(e){
-    var archivo= e.target.files;
-    var output;
-    output.push(escape(f.name))
+function handleFileSelect(evt) {
+    var files = evt.target.files; 
+    for (var i = 0, f; f = files[i]; i++) {
+      
+        if (!f.type.match('video.*')) {
+            continue;
+        }
+        var reader = new FileReader();
+        reader.onload = (function (theFile) {
+            return function (e) {
+                var span = document.createElement('span');
+                span.innerHTML = ['<video class="thumb" src="',
+                    e.target.result,
+                    '" title="', escape(theFile.name),
+                    '"></video>'
+                ].join('');
+                document.getElementById('list').insertBefore(span, null);
+            };
+        })(f);
+        reader.readAsDataURL(f);
+    }
 }
+document.getElementById('files').addEventListener('change',
+    handleFileSelect, false);
