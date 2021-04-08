6239573d969f91e1af8d0fdfc9297f609f0d1455
Rocío Beato Martín <rociobeato91@gmail.com>
2021-04-08 10:17:47 +0200

    mensaje carga

--- javascript.js
+++ javascript.js
@@ -1,30 +1,76 @@
-if (window.File && window.FileReader && window.FileList) {
-    console.log('Todas las API son soportadas');
-} else {
-    alert('La API de FILE no es soportada en este navegador');
-}
 
+//Función para cargar el vídeo y mostrarlo
 function handleFileSelect(evt) {
     var files = evt.target.files; 
+
     for (var i = 0, f; f = files[i]; i++) {
       
         if (!f.type.match('video.*')) {
-            continue;
+            alert("Error. No es un archivo de video");
+            break;
         }
+        
         var reader = new FileReader();
         reader.onload = (function (theFile) {
             return function (e) {
-                var span = document.createElement('span');
-                span.innerHTML = ['<video class="thumb" src="',
+                var div = document.createElement('div');
+                div.innerHTML = ['<video id="video" oncanplay="finCarga()" src="',
                     e.target.result,
                     '" title="', escape(theFile.name),
                     '"></video>'
                 ].join('');
-                document.getElementById('list').insertBefore(span, null);
+                document.getElementById('salida').insertBefore(div, null);
             };
         })(f);
         reader.readAsDataURL(f);
     }
 }
-document.getElementById('files').addEventListener('change',
+document.getElementById('seleccionArchivos').addEventListener('change',
     handleFileSelect, false);
+    
+
+    //Funciones para controlar el vídeo
+    function Reproducir(){
+        var video = document.getElementById('video');
+        video.play();
+    }
+
+    function Parar(){
+        video.pause();
+    }
+
+    function masVolumen(){
+        video.volume+=0.1;
+    }
+
+    function menosVolumen(){
+        video.volume-=0.1;
+    }
+       
+
+  /*function cargando(){
+        var mensaje = document.getElementById('mensaje').innerHTML = 'Espere. Cargando vídeo...';
+    } 
+
+    function fin(){
+        mensaje.insertBefore = " ";
+    }*/
+
+    function cargando(){
+        var mensaje = document.getElementById('mensaje');
+        mensaje.innerHTML = "Cargando video...Espere";
+    }
+
+    function finCarga(){
+        mensaje.style.color ='#00ff37';
+        mensaje.innerHTML = "Ya puede reproducir el vídeo";
+    }
+
+
+
+
+
+    
+    
+
+
